d249213143586fc8381005b52a189d3516f735f1
LN-5 <ln-5@LN-5s-Mac-mini.local>
2017-02-24 11:31:27 +0530

    first commit

---
+++ Example/Pods/Target Support Files/Pods-Validmail_Tests/Pods-Validmail_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Validmail_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Validmail_Tests
+@end
---
+++ Example/Pods/Target Support Files/Validmail/Validmail-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Validmail : NSObject
+@end
+@implementation PodsDummy_Validmail
+@end
